18376565348009d7efdc8971f32b587879b11fe2
meera-m-t <sameerah.talafha@siu.edu>
2022-04-20 17:42:02 -0500

    fix gan code

--- ACGAN/acgan.py
+++ ACGAN/acgan.py
@@ -7,7 +7,6 @@ from keras.models import Model, Sequential
 from keras.layers.core import Reshape, Dense, Dropout, Flatten
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import Conv2D, UpSampling2D
-from keras.datasets import mnist
 from keras.optimizers import Adam
 from keras import backend as K
 from keras import initializers
@@ -17,9 +16,8 @@ K.common.set_image_dim_ordering('th')
 from PIL import Image
 latent_dim = 140+2
 
-#(X_train, y_train), (X_test, y_test) = mnist.load_data()
-data = np.load(r"/media/Data/CNNtest/5Cdata/data5C.npy") # 5 channels data,(num,100,100,5), channel 0-4 is DEM,RGB,NIR
-labels = np.load(r"/media/Data/CNNtest/5Cdata/label.npy")
+data = np.load(r"5Cdata/data5C.npy") # 5 channels data,(num,100,100,5), channel 0-4 is DEM,RGB,NIR
+labels = np.load(r"5Cdata/label.npy")
 data=data[:,:,:,:1]
 data1=[]
 k=np.zeros((100,100,1))
@@ -41,13 +39,13 @@ X_train,X_test,y_train,y_test = train_test_split(data1,labels,test_size=0.01,ran
 print(X_train.shape)
 print(y_train.shape)
 X_train = (X_train.astype(np.float32)*255 - 127.5)/127.5
-X_test = (X_test.astype(np.float32)*255 - 127.5)/127.5
+X_test= (X_test.astype(np.float32)*255 - 127.5)/127.5
 print(X_train[0])
 X_train = X_train.reshape(X_train.shape[0],1,100,100)
-X_train = X_test.reshape(X_test.shape[0],1,100,100)
+X_test = X_test.reshape(X_test.shape[0],1,100,100)
 print(X_train.shape)
 y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
+y_test= to_categorical(y_test)
 adam = Adam(lr=0.0002, beta_1=0.5)
 
 generator = Sequential()
@@ -59,10 +57,9 @@ generator.add(Reshape((128, 25, 25)))
 generator.add(UpSampling2D(size=(2, 2)))
 generator.add(Conv2D(64, kernel_size=(5, 5), padding='same'))
 generator.add(LeakyReLU(0.2))
-generator.add(BatchNormalization())
 generator.add(UpSampling2D(size=(2, 2)))
 
-# Produces a 28 × 28 1-channel feature map (shape of a MNIST image)
+# Produces a 28 × 28 1-channel feature map (shape of a  image)
 generator.add(Conv2D(1, kernel_size=(5, 5), padding='same', activation='tanh'))
 print(generator.summary())
 generator.compile(loss='binary_crossentropy', optimizer=adam)
@@ -72,11 +69,9 @@ discriminator = Sequential()
 discriminator.add(Conv2D(64, kernel_size=(5, 5), strides=(2, 2), padding='same', 
                          input_shape=(1, 100, 100), kernel_initializer=initializers.RandomNormal(stddev=0.02)))
 discriminator.add(LeakyReLU(0.2))
-discriminator.add(BatchNormalization())
 discriminator.add(Dropout(0.3))
 discriminator.add(Conv2D(128, kernel_size=(5, 5), strides=(2, 2), padding='same'))
 discriminator.add(LeakyReLU(0.2))
-discriminator.add(BatchNormalization())
 discriminator.add(Dropout(0.3))
 discriminator.add(Flatten())
 discriminator.add(Dense(1, activation='sigmoid',name="dis_output"))
@@ -87,13 +82,10 @@ discriminator.compile(loss='binary_crossentropy', optimizer=adam)
 classifier = Sequential()
 classifier.add(Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same',
                  input_shape=(1,100,100)))
-classifier.add(BatchNormalization())
 classifier.add(MaxPooling2D(pool_size=(2,2)))
 classifier.add(Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same'))
-classifier.add(BatchNormalization())
 classifier.add(MaxPooling2D(pool_size=(2,2)))
 classifier.add(Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same'))
-classifier.add(BatchNormalization())
 classifier.add(MaxPooling2D(pool_size=(2,2)))    
 classifier.add(Flatten())
 classifier.add(Dense(128, activation='relu'))
@@ -131,7 +123,7 @@ def plotLoss(epoch):
     plt.legend()
     plt.savefig('images/dcgan_loss_epoch_%d.png' % epoch)
 
-# Create a wall of generated MNIST images
+# Create a wall of generated images
 def plotGeneratedImages(epoch, examples=100, dim=(10, 10), figsize=(10, 10)):
     noise = np.random.normal(0, 1, size=[examples, latent_dim-2])
     labels = None
@@ -157,15 +149,6 @@ def plotGeneratedImages(epoch, examples=100, dim=(10, 10), figsize=(10, 10)):
             'plot_epoch_{0:03d}_generated.png'.format(epoch))
 
 
-
-    # plt.figure(figsize=figsize)
-    # for i in range(generatedImages.shape[0]):
-    #     plt.subplot(dim[0], dim[1], i+1)
-    #     plt.imshow(generatedImages[i, 0], interpolation='nearest', cmap='gray_r')
-    #     plt.axis('off')
-    # plt.tight_layout()
-    # plt.savefig('images/dcgan_generated_image_epoch_%d.png' % epoch)
-
 # Save the generator and discriminator networks (and weights) for later use
 def saveModels(epoch):
     generator.save('models/dcgan_generator_epoch_%d.h5' % epoch)
@@ -177,7 +160,7 @@ After each consecutive Epoch we can see how synthetic images being improved
 """
 
 epochs = 100
-batchSize = 128
+batchSize = 10
 batchCount = X_train.shape[0] / batchSize
 
 print('Epochs:', epochs)
@@ -196,7 +179,7 @@ for e in range(1, epochs+1):
         np.save("images1/orignal_%d.npy"%i,imgset)
         imgset=np.array(labels)
         np.save("images2/lables_%d.npy"%i,imgset)      
-        # Generate fake MNIST images
+        # Generate fake  images
         generatedImages = generator.predict(noise)
         X = np.concatenate([imageBatch, generatedImages])
 
@@ -225,9 +208,9 @@ for e in range(1, epochs+1):
     dLosses.append(dloss)
     gLosses.append(gloss)
     cLosses.append(closs)
-    # score = classifier.evaluate(X_test, y_test, verbose=0)
-    # print('Test loss:', score[0])
-    # print('Test accuracy:', score[1])
+    score = classifier.evaluate(X_test, y_test, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1]) 
     if e == 1 or e % 1 == 0:
         # Plot losses from every epoch
         plotGeneratedImages(e)
--- README.md
+++ README.md
@@ -4,8 +4,13 @@ This project of the paper [here](https://ieeexplore.ieee.org/abstract/document/9
 - Siamese Neural Networks (SNNs),
 - Capsule Networks (CapsNets),
 - Graph Convolutional Networks (GCNs),
-- Auxiliary Classifier GAN (AC-GAN) for augmentation data.
+- Auxiliary Classifier GAN (AC-GAN) as a data augmentation method.
 - also some traditional Machine Learning (ML) classifies.
 
+# Auxiliary Classifier GAN (AC-GAN)
+for data 
+```bash
+python acgan.py
+```
 # Dataset 
 you can upload the dataset and the prepared dataset for GAN from [here](https://www.dropbox.com/sh/o5jsenqhpbt5jwt/AAC6Yv0lhPqVa9gSc_uGBGRQa?dl=0)
